557730d9fc5bea14ac84b6953aee5b454ed5d8e1
Bijay Shrestha <7867129+bijay-shrestha@users.noreply.github.com>
2025-12-09 20:55:52 -0500

    Initial Commit

---
+++ src/main/java/com/fifteendays/coding/FifteenDaysCodingApplication.java
@@ -0,0 +1,13 @@
+package com.fifteendays.coding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FifteenDaysCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FifteenDaysCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fifteendays/coding/FifteenDaysCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifteendays.coding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FifteenDaysCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
